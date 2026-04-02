5fd29b736b067a6ad952c83921a5c379acffacfd
lars <lars@larss-MacBook-Pro.local>
2026-04-02 14:42:37 +0200

    feat: switch to multilingual Jina reranker for Norwegian content

--- src/reranker.py
+++ src/reranker.py
@@ -2,7 +2,7 @@ from fastembed.rerank.cross_encoder import TextCrossEncoder
 
 
 class Reranker:
-    def __init__(self, model_name: str = "Xenova/ms-marco-MiniLM-L-6-v2"):
+    def __init__(self, model_name: str = "jinaai/jina-reranker-v2-base-multilingual"):
         self._model = TextCrossEncoder(model_name=model_name)
 
     def rerank(
